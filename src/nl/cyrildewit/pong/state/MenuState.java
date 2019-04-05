715bd70792fe7dba0e00d25bd76ee9346613eaf2
Cyril de Wit <cyrilwit@gmail.com>
2019-04-05 09:36:20 +0200

    feat: add MenuState

---
+++ src/nl/cyrildewit/pong/state/MenuState.java
@@ -0,0 +1,23 @@
+package nl.cyrildewit.pong.state;
+
+import java.awt.Graphics;
+
+import nl.cyrildewit.pong.Game;
+
+public class MenuState extends State {
+
+	public MenuState(Game game) {
+		super(game);
+	}
+
+	@Override
+	public void tick() {
+		//
+	}
+
+	@Override
+	public void render(Graphics g) {
+		//
+	}
+
+}
