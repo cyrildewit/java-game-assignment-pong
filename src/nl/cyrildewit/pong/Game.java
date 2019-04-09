d2f44f96427d9015129f0c63d06f461d2dbe245a
Cyril de Wit <cyrilwit@gmail.com>
2019-04-09 21:00:55 +0200

    style: step forward to cleaner code

--- src/nl/cyrildewit/pong/Game.java
+++ src/nl/cyrildewit/pong/Game.java
@@ -9,15 +9,14 @@ import nl.cyrildewit.pong.state.GameState;
 import nl.cyrildewit.pong.state.MenuState;
 import nl.cyrildewit.pong.state.State;
 
-public class Game implements Runnable {
-
-    private boolean dev = false;
+public class Game implements Runnable
+{
+    private boolean running = false;
+    private int width = 320, height = 240;
+    private float scale = 1f;
+    private String title;
 
 	private Display display;
-	public String title;
-	public int width, height;
-
-	private boolean running = false;
 	private Thread thread;
 
 	private BufferStrategy displayBufferStrategy;
@@ -25,7 +24,6 @@ public class Game implements Runnable {
 
 	// State
 	private State gameState;
-	private State menuState;
 
 	// Input
 	private KeyManager keyManager;
@@ -33,14 +31,80 @@ public class Game implements Runnable {
 	// Handler
 	private Handler handler;
 
-	public Game(String title, int width, int height) {
+    public Game(String title, int width, int height)
+    {
 		this.title = title;
 		this.width = width;
 		this.height = height;
 		keyManager = new KeyManager();
-	}
+    }
 
-	private void init() {
+    public synchronized void start()
+    {
+        if (running) {
+            return;
+        }
+
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop()
+    {
+        if (!running) {
+            return;
+        }
+
+        running = false;
+
+        try {
+            thread.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run()
+    {
+        init();
+
+        int fps = 60;
+        double timePerTick = 1000000000 / fps;
+        double delta = 0;
+        long now;
+        long lastTime = System.nanoTime();
+        long timer = 0;
+        int ticks = 0;
+
+        while (running) {
+            now = System.nanoTime();
+            delta += (now - lastTime) / timePerTick;
+            timer += now - lastTime;
+            lastTime = now;
+
+            if (delta >= 1) {
+                tick();
+                render();
+                ticks++;
+                delta--;
+            }
+
+            if (timer >= 1000000000) {
+                if (dev) {
+                    System.out.println("Ticks and Frames: " + ticks);
+                }
+                ticks = 0;
+                timer = 0;
+            }
+        }
+
+        stop();
+    }
+
+    private void init()
+    {
 		display = new Display(title, width, height);
 		display.getFrame().addKeyListener(keyManager);
 
@@ -51,7 +115,8 @@ public class Game implements Runnable {
 		State.setState(gameState);
 	}
 
-	private void tick() {
+    private void tick()
+    {
 		keyManager.tick();
 
         if (State.getState() != null) {
@@ -59,7 +124,8 @@ public class Game implements Runnable {
         }
 	}
 
-	private void render() {
+    private void render()
+    {
 		displayBufferStrategy = display.getCanvas().getBufferStrategy();
 
 		if (displayBufferStrategy == null) {
@@ -78,42 +144,7 @@ public class Game implements Runnable {
 		g.dispose();
 	}
 
-	@Override
-	public void run() {
-		init();
 
-		int fps = 60;
-		double timePerTick = 1000000000 / fps;
-		double delta = 0;
-		long now;
-		long lastTime = System.nanoTime();
-		long timer = 0;
-		int ticks = 0;
-
-		while (running) {
-			now = System.nanoTime();
-			delta += (now - lastTime) / timePerTick;
-			timer += now - lastTime;
-			lastTime = now;
-
-			if (delta >= 1) {
-				tick();
-				render();
-				ticks++;
-				delta--;
-			}
-
-			if (timer >= 1000000000) {
-				if (dev) {
-                    System.out.println("Ticks and Frames: " + ticks);
-                }
-                ticks = 0;
-				timer = 0;
-			}
-		}
-
-		stop();
-	}
 
 	public KeyManager getKeyManager() {
 		return keyManager;
@@ -125,30 +156,6 @@ public class Game implements Runnable {
 
 	public int getHeight() {
 		return height;
-	}
-
-	public synchronized void start() {
-		if (running) {
-			return;
-		}
-
-		running = true;
-		thread = new Thread(this);
-		thread.start();
-	}
-
-	public synchronized void stop() {
-		if (! running) {
-			return;
-		}
-
-		running = false;
-
-		try {
-			thread.join();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+    }
 
 }
