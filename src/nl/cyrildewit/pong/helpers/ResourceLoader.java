77fe0f1f04106be26e7063267acc6e64e9fbbcc1
Cyril de Wit <cyrilwit@gmail.com>
2019-05-16 12:53:22 +0200

    feat: add ResourceLoader

--- src/nl/cyrildewit/pong/helpers/ResourceLoader.java
+++ src/nl/cyrildewit/pong/helpers/ResourceLoader.java
@@ -5,11 +5,11 @@ import java.io.InputStream;
 public class ResourceLoader {
 	public static InputStream load(String path) {
 		InputStream input = ResourceLoader.class.getResourceAsStream(path);
-		
+
 		if(input == null) {
 			input = ResourceLoader.class.getResourceAsStream("/" + path);
 		}
-		
+
 		return input;
 	}
 }
